9ea77e66a015e2790187d48115a763f87b7747af
Jake Bellacera <hi@jakebellacera.com>
2024-10-26 20:18:11 +0000

    Add linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,25 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { includeIgnoreFile } from "@eslint/compat";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const packagesWithGitignores = ["examples/docusaurus"];
 
 export default [
+  ...packagesWithGitignores.map((f) =>
+    includeIgnoreFile(path.resolve(__dirname, f, ".gitignore")),
+  ),
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
 ];
